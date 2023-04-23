17732e99f8c8dc6104a82e367722dd4e9fe45d06
Ильдар Закиров <ildar.zak.v@gmail.com>
2023-04-23 22:30:14 +0300

    scroll fix

--- src/scripts/world/controls.ts
+++ src/scripts/world/controls.ts
@@ -11,7 +11,7 @@ export class Controls {
         GSAP.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
         if (
-            !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
                 navigator.userAgent
             )
         ) {
